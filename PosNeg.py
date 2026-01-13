728e1e94f31e28b0d8a4cb600d19d9d04680d72c
srusxtikulkarni <shrushtikulkarni670@gmail.com>
2026-01-13 13:36:26 +0530

    Add files via upload

---
+++ PosNeg.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number to check:"))
+if num<0:
+  print("Number is negative")
+elif num>0:
+  print("Number is positive")
+elif num==0:
+  print("Number is zero")
+else:
+  print("Number is Invalid")
